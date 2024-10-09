b033ef54614811065bdc4269dc589776fa77eae2
Yiming0576 <Yiming0576@gmail.com>
2024-10-09 11:38:53 -0400

    delete the exercise one

--- wb3Demo/src/main/java/com/pluralsight/Demo2.java
+++ wb3Demo/src/main/java/com/pluralsight/Demo2.java
@@ -1,2 +1,24 @@
-package com.pluralsight;public class Demo2 {
+package com.pluralsight;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Demo2 {
+
+    public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(System.in); FileInputStream fileInputStream = new FileInputStream("peom.txt")){
+
+            int lineNumber = 1;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.printf("%d: %s\n", lineNumber++, line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
